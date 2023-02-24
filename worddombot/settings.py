c909ac0186ed6cd4fb6ef98ad5952a8a817ae2fd
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-02-24 17:25:03 +0500

    2.24

--- worddombot/settings.py
+++ worddombot/settings.py
@@ -127,4 +127,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
